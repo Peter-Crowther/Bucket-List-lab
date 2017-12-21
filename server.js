a618d9e3a0704d5769995bdeabeb9fc1af6e70e9
Peter Crowther <petercrowther90@hotmail.com>
2017-12-21 09:47:51 +0000

    db and site up and running

--- server.js
+++ server.js
@@ -8,16 +8,72 @@ server.use(express.static('client/build'));
 server.use(parser.urlencoded({extended: true}));
 server.use(parser.json());
 
-server.get('/countries', function(req, res) {
-  console.log(req.body);
-});
 
-server.post('/countries', function(req, res) {
-  res.status(201);
-  res.json({name: "hi"});
-});
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const db = client.db("bucketlist");
+
+  console.log("Connected to database");
+
+  server.post('/countries', function(req, res) {
+    db.collection('countries').insert(req.body, function(err, result) {
+      if (err) {
+        console.log(err);
+        res.status(500);
+        res.send();
+        return;
+      }
+      console.log('Saved to database');
+      res.status(201);
+      res.json(result.ops[0]);
+    });
+  });
+
+  server.get('/countries', function(req, res) {
 
 
-server.listen(3000, function() {
-  console.log("Listening on port 3000...");
+    db.collection('countries').find().toArray(function(err, result) {
+      if (err) {
+        res.status(500);
+        console.log(err);
+        res.send();
+        return;
+      }
+      res.json(result);
+    });
+  });
+
+  server.delete('/countries', function(req, res) {
+    db.collection('country').remove({}, function(err, result) {
+      if(err) {
+        console.log(err);
+        res.status(500);
+        res.send();
+        return;
+      }
+
+      res.status(204);
+      res.send();
+    });
+  });
+
+  server.listen(3000, function(){
+    console.log("Listening on port 3000");
+  });
 });
+// server.get('/countries', function(req, res) {
+//   console.log(req.body);
+// });
+//
+// server.post('/countries', function(req, res) {
+//   res.status(201);
+//   res.json({name: "hi"});
+// });
+//
+//
+// server.listen(3000, function() {
+//   console.log("Listening on port 3000...");
+// });
